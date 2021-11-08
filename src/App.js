2d95590343d6db030d0392cc9d2fcccc5550fe79
JoaoNogueira99 <85769366+JoaoNogueira99@users.noreply.github.com>
2021-11-08 19:01:33 +0000

    Updating Files
    Counter is working, update files.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import React, { Component }  from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css';
+import Counter from './counter';
+
+function App() {
+  return (
+    <div className="App">
+      < Counter />
+    </div>
+  );
+}
+
+export default App;
